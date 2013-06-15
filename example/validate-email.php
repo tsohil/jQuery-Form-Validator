d68b77f2de098f779058d84ae8db11d928e4f280
Victor Jonsson <kontakt@victorjonsson.se>
2013-06-15 15:14:57 +0200

    removed example

--- example/validate-email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$json = array('success' => 'false', 'message'=>'This value has to be &quot;hello&quot;');
-if( isset($_REQUEST['validate']) ) {
-    $json['success'] = $_REQUEST['validate'] == 'hello';
-}
-
-sleep(2);
-
-echo json_encode($json);
